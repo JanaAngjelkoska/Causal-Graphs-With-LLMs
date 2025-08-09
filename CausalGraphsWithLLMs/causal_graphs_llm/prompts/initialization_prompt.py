6b31def82eff3a9b043601de6baf98e0724314aa
Jana <jana.angjelkoska@hotmail.com>
2025-08-09 12:01:44 +0200

    Refactored folder structure

---
+++ CausalGraphsWithLLMs/causal_graphs_llm/prompts/initialization_prompt.py
@@ -0,0 +1,11 @@
+BASE_PROMPT = """
+ You're a helpful AI assistant to a...
+"""
+
+EXPERT_OPINION_PROMPT = """
+According to X ...
+"""
+
+
+def create_extraction_prompt(baseline: str) -> str:
+    pass
